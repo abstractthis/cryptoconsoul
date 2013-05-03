aaa47cca9ee2989cc6f989e6640c8af1d14dec3e
dshmif <dshmif@gmail.com>
2013-05-02 22:15:58 -0400

    Comments added.

--- src/main/java/org/caiedea/cryptoconsoul/biz/DecryptionService.java
+++ src/main/java/org/caiedea/cryptoconsoul/biz/DecryptionService.java
@@ -8,9 +8,28 @@ import org.caiedea.cryptopression.decrypt.FilePassThroughDecryptor;
 import org.caiedea.cryptopression.decrypt.aes.FileAesDecryptor;
 import org.caiedea.cryptopression.decrypt.decompress.DecompressFileDecryptor;
 
+/**
+ * Handles the interaction with the cryptopression library to perform
+ * the decryption.
+ * 
+ * @author dsmith
+ *
+ */
 public class DecryptionService {
 	private static final String CRYPTO_LIB_INPUT_PROP_KEY = "cryptopression.keepRawInput";
 	
+	/**
+	 * Decrypts the <code>File</code> located at <code>targetPath</code> and places
+	 * the result in a <code>File</code> located at <code>resultPath</code>. If
+	 * <code>decompress</code> is <code>true</code> the <code>File</code> located at
+	 * <code>targetPath</code> will be inflated after it's decrypted. If
+	 * <code>rmInput</code> is <code>true</code> the encrypted file will be
+	 * deleted after successful decryption.
+	 * @param targetPath
+	 * @param resultPath
+	 * @param decompress
+	 * @param rmInput
+	 */
 	public void decryptFile(String targetPath, String resultPath, boolean decompress, boolean rmInput) {
 		try {
 			// If the input should be deleted set it so in the cryptopression lib
